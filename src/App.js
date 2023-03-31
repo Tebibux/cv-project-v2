405f4c2ed286b4d665c2ac3532f0d886f511dd10
tebibux <tebibusolomon79@gmail.com>
2023-03-31 12:24:30 +0300

    structured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import React, { Component } from "react";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+	render() {
+		return (
+			<header>
+				<h1>
+					Build Your CV
+				</h1>
+				<button>Preview</button>
+			</header>
+		);
+	}
+}
+
+export default Header;
