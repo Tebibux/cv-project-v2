0dc4a2eb7515d18abbb96064b86b8189845d2ce0
tebibux <tebibusolomon79@gmail.com>
2023-04-01 09:20:26 +0300

    profile style done!

--- src/components/main/childComponent/Profile.js
+++ src/components/main/childComponent/Profile.js
@@ -8,18 +8,19 @@ class Profile extends Component {
 					<div className="image">
 						<input type="file" name="userImage"
 							accept="image/png, image/gif, image/jpeg" />
+						{/* Add placeholder here for the image */}
 					</div>
 					<div className="disc">
-						<input type="text" name="userName" />
-						<input type="text" name="userBio" />
+						<input type="text" name="userName" placeholder="Your Name"/>
+						<input type="text" name="userBio" placeholder="Small bio about yourself here"/>
 					</div>
 				</div>
 				<div className="userAddress">
 					<form >
-						<input type="tel" name="userPhone" />
-						<input type="text" name="userLocation" />
-						<input type="email" name="userEmail" />
-						<input type="url" name="linkedIn" />
+						<input type="tel" name="userPhone" placeholder="You'r Phone Here"/>
+						<input type="text" name="userLocation" placeholder="Your current location here"/>
+						<input type="email" name="userEmail" placeholder="Your email here" />
+						<input type="url" name="linkedIn" placeholder="Your linkedIn here"/>
 					</form>
 				</div>
 			</div>
