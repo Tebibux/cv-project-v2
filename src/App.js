c9e974fe2a279b76bad951709848a3e68072023b
tebibux <tebibusolomon79@gmail.com>
2023-04-10 07:31:06 +0300

    trying to upload the image

--- src/App.js
+++ src/App.js
@@ -8,92 +8,16 @@ class App extends Component {
   constructor() {
     super();
   };
-  previewImage = (e) => {
-    let fileInput = document.getElementById('image-selection');
-    fileInput.style.display = 'none';
-    console.log(fileInput)
-  }
+  // previewImage = (e) => {
+  //   let fileInput = document.getElementById('image-selection');
+  //   fileInput.style.display = 'none';
+  //   console.log(fileInput)
+  // }
   render() {
     return (
       <div className="container">
         <Header />
-        <div className="main">
-          {/* profile starts Here */}
-          <div className="profile">
-            <div className="userProfile">
-              <div className="image-div">
-                <label htmlFor="fileInput" id="image-selection">
-                  <input type="file" accept=".jpg, .jpeg, .png"
-                    name="fileInput" id="fileInput" onChange={this.previewImage} />
-                </label>
-
-                {/* Add placeholder here for the image */}
-              </div>
-              <div className="disc">
-                <input type="text" name="userName" placeholder="Your Name" />
-                <input type="text" name="userBio" placeholder="Small bio about yourself here" />
-              </div>
-            </div>
-            <div className="userAddress">
-              <form >
-                <input type="tel" name="userPhone" placeholder="You'r Phone Here" />
-                <input type="text" name="userLocation" placeholder="Your current location here" />
-                <input type="email" name="userEmail" placeholder="Your email here" />
-                <input type="url" name="linkedIn" placeholder="Your linkedIn here" />
-              </form>
-            </div>
-          </div>
-          {/* profile ends Here */}
-
-          {/* Education states here */}
-          <div className="userEducation">
-            <div className="title">Education</div>
-            <ul className="educations">
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-            </ul>
-            <div className="user-education-dic" >
-              <input type="text" name="education" placeholder="Add your Educations here" />
-              <button className="btn-add">Add</button>
-            </div>
-          </div>
-          {/* education ends Here */}
-
-          {/* experience starts Here */}
-          <div className="userExperience">
-            <div className="title">Experience</div>
-            <ul>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-            </ul>
-            <div className="user-experience-dic" >
-              <input type="text" name="userExperience" placeholder="Add your Experience here" />
-              <button className="btn-add">Add</button>
-            </div>
-          </div>
-          {/* experience ends Here */}
-          {/* skill starts Here */}
-          <div className="userSkill">
-            <div className="title">Skill</div>
-            <ul>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam beatae numquam, </li>
-            </ul>
-            <div className="user-skill-dic" >
-              <input type="text" name="userSkill" placeholder="Add your skills here" />
-              <button className="btn-add">Add</button>
-            </div>
-          </div>
-          {/* skill ends Here */}
-        </div>
+        <Main />
         <Footer />
       </div>
 
--- src/components/main/childComponent/Profile.js
+++ src/components/main/childComponent/Profile.js
@@ -1,31 +1,108 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 class Profile extends Component {
+	constructor() {
+		super();
+		this.state = {
+			userImg: null,
+		}
+	};
+	previewImage = (e) => {
+		let fileInput = document.getElementById('image-selection');
+		// eslint-disable-next-line no-lone-blocks
+		fileInput.style.display = 'none'; {/* this disable the image button */ }
+		let imageDisplayBox = document.getElementById('image-div')
+		let reader = new FileReader();
+		reader.onload = () => reader.result;
+		let userImage = document.createElement('img');
+		userImage.src = reader.onload;
+		imageDisplayBox.appendChild(userImage);
+		reader.readAsDataURL(e.target.files[0]);
+
+	}
 	render() {
 		return (
 			<div className="profile">
+				{/* profile starts Here */}
 				<div className="userProfile">
-					<div className="image">
-						<input type="file" name="userImage"
-							accept="image/png, image/gif, image/jpeg" />
+					<div id="image-div">
+						<label htmlFor="fileInput" id="image-selection">
+							<input type="file" accept=".jpg, .jpeg, .png"
+								name="fileInput" id="fileInput" onChange={this.previewImage} />
+						</label>
+
 						{/* Add placeholder here for the image */}
 					</div>
 					<div className="disc">
-						<input type="text" name="userName" placeholder="Your Name"/>
-						<input type="text" name="userBio" placeholder="Small bio about yourself here"/>
+						<input type="text" name="userName" placeholder="Your Name" />
+						<input type="text" name="userBio" placeholder="Small bio about yourself here" />
 					</div>
 				</div>
 				<div className="userAddress">
 					<form >
-						<input type="tel" name="userPhone" placeholder="You'r Phone Here"/>
-						<input type="text" name="userLocation" placeholder="Your current location here"/>
+						<input type="tel" name="userPhone" placeholder="You'r Phone Here" />
+						<input type="text" name="userLocation" placeholder="Your current location here" />
 						<input type="email" name="userEmail" placeholder="Your email here" />
-						<input type="url" name="linkedIn" placeholder="Your linkedIn here"/>
+						<input type="url" name="linkedIn" placeholder="Your linkedIn here" />
 					</form>
 				</div>
+				{/* profile ends Here */}
 			</div>
+
 		);
 	}
 }
 
 export default Profile;
+<div className="main">
+
+
+	{/* Education states here */}
+	<div className="userEducation">
+		<div className="title">Education</div>
+		<ul className="educations">
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+		</ul>
+		<div className="user-education-dic" >
+			<input type="text" name="education" placeholder="Add your Educations here" />
+			<button className="btn-add">Add</button>
+		</div>
+	</div>
+	{/* education ends Here */}
+
+	{/* experience starts Here */}
+	<div className="userExperience">
+		<div className="title">Experience</div>
+		<ul>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+		</ul>
+		<div className="user-experience-dic" >
+			<input type="text" name="userExperience" placeholder="Add your Experience here" />
+			<button className="btn-add">Add</button>
+		</div>
+	</div>
+	{/* experience ends Here */}
+	{/* skill starts Here */}
+	<div className="userSkill">
+		<div className="title">Skill</div>
+		<ul>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+			<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				Nam beatae numquam, </li>
+		</ul>
+		<div className="user-skill-dic" >
+			<input type="text" name="userSkill" placeholder="Add your skills here" />
+			<button className="btn-add">Add</button>
+		</div>
+	</div>
+	{/* skill ends Here */}
+</div>
