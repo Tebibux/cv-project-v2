94abd22a303a1c3ed46ed8f767c1b0c32707b1ba
tebibux <tebibusolomon79@gmail.com>
2023-04-12 07:03:42 +0300

    some UI changed

--- src/components/main/childComponent/Profile.js
+++ src/components/main/childComponent/Profile.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 
 class Profile extends Component {
 	constructor() {
@@ -8,33 +8,33 @@ class Profile extends Component {
 		}
 	};
 	previewImage = () => {
-			let fileInput = document.getElementById('fileInput');
-			let imageDiv = document.getElementById('image-div');
-			let label = document.getElementById('image-selection');
-			// Get the selected image
-			fileInput.addEventListener('change', () => {
-				const imageFile  = fileInput.files[0];
-				if(imageFile){
-					const reader = new FileReader();
-					reader.readAsDataURL(imageFile);
-					reader.onload = () => {
-						const imgData = reader.result;
-						localStorage.setItem('imageData', imgData);
-						const userImage = new Image();
-						userImage.setAttribute('src', imgData);
-						userImage.setAttribute('id', 'userImage')
-						imageDiv.appendChild(userImage);
-						label.style.display = 'none'
-					};
-					reader.onerror = (error) => {
-						console.error('Error: ', error);
-					}
-				} else {
-					console.warn('No file Selected')
+		let fileInput = document.getElementById('fileInput');
+		let imageDiv = document.getElementById('image-div');
+		let label = document.getElementById('image-selection');
+		// Get the selected image
+		fileInput.addEventListener('change', () => {
+			const imageFile = fileInput.files[0];
+			if (imageFile) {
+				const reader = new FileReader();
+				reader.readAsDataURL(imageFile);
+				reader.onload = () => {
+					const imgData = reader.result;
+					localStorage.setItem('imageData', imgData);
+					const userImage = new Image();
+					userImage.setAttribute('src', imgData);
+					userImage.setAttribute('id', 'userImage')
+					imageDiv.appendChild(userImage);
+					label.style.display = 'none'
+				};
+				reader.onerror = (error) => {
+					console.error('Error: ', error);
 				}
-				
-			})
-				
+			} else {
+				console.warn('No file Selected')
+			}
+
+		})
+
 	}
 	render() {
 		return (
@@ -44,14 +44,16 @@ class Profile extends Component {
 					<div id="image-div">
 						<label htmlFor="fileInput" id="image-selection">
 							<input type="file" accept=".jpg, .jpeg, .png"
-								name="fileInput" id="fileInput" onClick={this.previewImage}/>
+								name="fileInput" id="fileInput" onClick={this.previewImage} />
 						</label>
 
 						{/* Add placeholder here for the image */}
 					</div>
 					<div className="disc">
-						<input type="text" name="userName" placeholder="Your Name" />
-						<input type="text" name="userBio" placeholder="Small bio about yourself here" />
+						<input type="text" name="userName"
+							placeholder="Your Name" id="userName" />
+						<textarea type="text" name="userBio" rows="4" cols="50"
+							placeholder="Small bio about yourself here" id="userBio" />
 					</div>
 				</div>
 				<div className="userAddress">
