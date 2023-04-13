0272a151b54b8c747282b64fd6d1454af00ed37b
tebibux <tebibusolomon79@gmail.com>
2023-04-13 06:31:43 +0300

    location and phone done

--- src/components/main/childComponent/Profile.js
+++ src/components/main/childComponent/Profile.js
@@ -8,6 +8,10 @@ class Profile extends Component {
 			inputNameValue: '',
 			isInputBioActive: true,
 			inputBioValue: '',
+			isInputUserPhoneActive: true,
+			inputUserPhoneValue: '',
+			isInputLocationActive: true,
+			inputUserLocationValue: '',
 		};
 	};
 	previewImage = () => {
@@ -67,9 +71,34 @@ class Profile extends Component {
 	}
 	// userBio //done if done
 
+	// userAddress starts here
+	// user phone starts here
+	handlePhoneDoubleClick = () => {
+		this.setState({ isInputUserPhoneActive: true });
+	}
+	handlePhoneBlur = () => {
+		this.setState({ isInputUserPhoneActive: false })
+	}
+	handlePhoneChange = (e) => {
+		this.setState({ inputUserPhoneValue: e.target.value })
+	}
+	// userPhone ends here
+	handleLocationDoubleClick = () => {
+		this.setState({ isInputLocationActive: true });
+	}
+	handleLocationBlur = () => {
+		this.setState({ isInputLocationActive: false })
+	}
+	handleLocationChange = (e) => {
+		this.setState({ inputUserLocationValue: e.target.value })
+	}
+	// userAddress ends here
 	render() {
 		const { isInputNameActive, inputNameValue,
-			isInputBioActive, inputBioValue } = this.state;
+			isInputBioActive, inputBioValue,
+			isInputUserPhoneActive, inputUserPhoneValue,
+			isInputLocationActive, inputUserLocationValue
+		} = this.state;
 		return (
 			<div className="profile">
 				{/* profile starts Here */}
@@ -85,7 +114,7 @@ class Profile extends Component {
 					{/* image input ends here */}
 					<div className="disc">
 						{
-							isInputNameActive || inputNameValue==='' ? (
+							isInputNameActive || inputNameValue === '' ? (
 								<input type="text" name="userName"
 									placeholder="Your Name" id="userName"
 									value={inputNameValue}
@@ -94,15 +123,15 @@ class Profile extends Component {
 
 								/>
 							) : (
-									<div className="userName"
-										onDoubleClick={this.handleNameDoubleClick} >
-										{inputNameValue}
-									</div>
-								)
+								<div className="userName"
+									onDoubleClick={this.handleNameDoubleClick} >
+									{inputNameValue}
+								</div>
+							)
 						}
 						{/* using the logic in the userName input render the Bio */}
 						{
-							isInputBioActive || inputBioValue==='' ? (
+							isInputBioActive || inputBioValue === '' ? (
 
 								<textarea type="text" name="userBio" rows="4" cols="50"
 									placeholder="Small bio about yourself here" id="userBio"
@@ -121,8 +150,40 @@ class Profile extends Component {
 				</div>
 				<div className="userAddress">
 					<form >
-						<input type="tel" name="userPhone" placeholder="You'r Phone Here" />
-						<input type="text" name="userLocation" placeholder="Your current location here" />
+						{
+							isInputUserPhoneActive || inputUserPhoneValue === '' ? (
+								<input type="tel" name="userPhone"
+									placeholder="You'r Phone Here"
+									value={inputUserPhoneValue}
+									onChange={this.handlePhoneChange}
+									onBlur={this.handlePhoneBlur}
+								/>
+							) : (
+								<div className="userPhone"
+									onDoubleClick={this.handlePhoneDoubleClick}
+								>
+									{inputUserPhoneValue}
+								</div>
+							)
+
+						}
+						{
+
+							isInputLocationActive || inputUserLocationValue === '' ? (
+								< input type="text" name="userLocation"
+									placeholder="Your current location here"
+									value={inputUserLocationValue}
+									onChange={this.handleLocationChange}
+									onBlur={this.handleLocationBlur}
+								/>
+							) : (
+								<div className="userLocation"
+									onDoubleClick={this.handleLocationDoubleClick}
+								>
+									{inputUserLocationValue}
+								</div>
+							)
+						}
 						<input type="email" name="userEmail" placeholder="Your email here" />
 						<input type="url" name="linkedIn" placeholder="Your linkedIn here" />
 					</form>
