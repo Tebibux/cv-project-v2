2bcfc7a9449e6958b1e2030a71ba9cc732f31749
tebibux <tebibusolomon79@gmail.com>
2023-04-14 07:12:57 +0300

    Education done

--- src/components/main/childComponent/Education.js
+++ src/components/main/childComponent/Education.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import uuid4 from "uuid4";
+import uuid from 'uuid4'
 
 class Education extends Component {
-	constructor(){
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			certification: {diploma: ''},
+			certification: { diploma: '' },
 			diplomas: [],
 		}
 	}
@@ -19,32 +21,39 @@ class Education extends Component {
 		e.preventDefault();
 		this.setState({
 			diplomas: this.state.diplomas.concat(this.state.certification),
-			certification: {diploma: ''}
+			certification: { diploma: '' }
 		})
 	}
 	render() {
-		const {certification, diplomas} = this.state;
+		const { certification, diplomas } = this.state;
+		const inputAddDocument = document.getElementById('listOfEducations')
 		return (
 			<div className="userEducation">
 				<div className="title">Education</div>
 				<ul className="educations">
-					<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-						Nam beatae numquam, </li>
-					
-					{diplomas.map((certification) => {
-						return <li>{certification.diploma}</li>
-					})}
-										
+					{
+						diplomas.map((certification) => {
+							return <li key={uuid4()}>{certification.diploma}</li>
+						})
+
+					}
 				</ul>
-				<div className="user-education-dic" >
-					<input type="text" name="education" placeholder="Add your Educations here"
-					onChange={this.handleChange}
-					value={certification}
-					/>
-					<button className="btn-add"
-						onClick={this.handleAdd}
-					>Add</button>
-				</div>
+				{
+					(diplomas.length < 4) ? (
+						<div className="user-education-dic" id="listOfEducations">
+							<input type="text" name="education" placeholder="Add your Educations here"
+								onChange={this.handleChange}
+								value={certification.diploma}
+							/>
+							<button className="btn-add"
+								onClick={this.handleAdd}
+							>Add</button>
+						</div>
+					) : (
+						inputAddDocument.style.display = ''
+					)
+				}
+
 			</div>
 		);
 	}
