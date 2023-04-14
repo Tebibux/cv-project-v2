059b14646c37f4d78248a65e05fe4dc31326c5a7
tebibux <tebibusolomon79@gmail.com>
2023-04-14 07:55:41 +0300

    finished

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-
+import '@fortawesome/fontawesome-free/css/all.css';
 class Footer extends Component {
 	render() {
 		return (
 			<footer className="footer">
 				<div className="footer-text">
-					<h4>TheOdinProject 2022 Tebibux ©</h4>
+					<h4>TheOdinProject 2022 Tebibux ©
+						<a href="https://github.com/Tebibux/cv-project">
+							<i className="fa fa-github" >
+							</i>
+						</a>
+					</h4>
 				</div>
 			</footer>
 		);
