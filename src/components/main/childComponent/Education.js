e4720b5cd37a9a407920b5ed32f6ed7db0a0b43a
tebibux <tebibusolomon79@gmail.com>
2023-04-14 06:23:41 +0300

    Some CSS changes

--- src/components/main/childComponent/Education.js
+++ src/components/main/childComponent/Education.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 
 class Education extends Component {
+	constructor(){
+		super();
+		this.state = {
+			certification: {diploma: ''},
+			diplomas: [],
+		}
+	}
+	handleChange = (e) => {
+		this.setState({
+			certification: {
+				diploma: e.target.value,
+			}
+		})
+	}
+	handleAdd = (e) => {
+		e.preventDefault();
+		this.setState({
+			diplomas: this.state.diplomas.concat(this.state.certification),
+			certification: {diploma: ''}
+		})
+	}
 	render() {
+		const {certification, diplomas} = this.state;
 		return (
 			<div className="userEducation">
 				<div className="title">Education</div>
 				<ul className="educations">
 					<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
 						Nam beatae numquam, </li>
-					<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-						Nam beatae numquam, </li>
-					<li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-						Nam beatae numquam, </li>						
+					
+					{diplomas.map((certification) => {
+						return <li>{certification.diploma}</li>
+					})}
+										
 				</ul>
 				<div className="user-education-dic" >
-					<input type="text" name="education" placeholder="Add your Educations here"/>
-					<button className="btn-add">Add</button>
+					<input type="text" name="education" placeholder="Add your Educations here"
+					onChange={this.handleChange}
+					value={certification}
+					/>
+					<button className="btn-add"
+						onClick={this.handleAdd}
+					>Add</button>
 				</div>
 			</div>
 		);
