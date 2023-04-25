6c09a26d4107cdba1d44f94263e249558d59b2bc
tebibux <tebibusolomon79@gmail.com>
2023-04-25 09:55:46 +0300

    Educations done

--- src/components/main/childComponent/Education.js
+++ src/components/main/childComponent/Education.js
@@ -1,61 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uuid4 from "uuid4";
 
-class Education extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			certification: { diploma: '' },
-			diplomas: [],
-		}
-	}
-	handleChange = (e) => {
-		this.setState({
+const Education = () => {
+	const { certification, setCertification } = useState({ diploma: '' });
+	const diplomas = [];
+	const inputAddDocument = document.getElementById('listOfEducations')
+
+	const handleChange = (e) => {
+		setCertification({
 			certification: {
 				diploma: e.target.value,
 			}
 		})
 	}
-	handleAdd = (e) => {
+	const handleAdd = (e) => {
 		e.preventDefault();
-		this.setState({
-			diplomas: this.state.diplomas.concat(this.state.certification),
+		setCertification({
+			diplomas: certification.diplomas.concat(this.state.certification),
 			certification: { diploma: '' }
 		})
 	}
-	render() {
-		const { certification, diplomas } = this.state;
-		const inputAddDocument = document.getElementById('listOfEducations')
-		return (
-			<div className="userEducation">
-				<div className="title">Education</div>
-				<ul className="educations">
-					{
-						diplomas.map((certification) => {
-							return <li key={uuid4()}>{certification.diploma}</li>
-						})
 
-					}
-				</ul>
+	return (
+		<div className="userEducation">
+			<div className="title">Education</div>
+			<ul className="educations">
 				{
-					(diplomas.length < 4) ? (
-						<div className="user-education-dic" id="listOfEducations">
-							<input type="text" name="education" placeholder="Add your Educations here"
-								onChange={this.handleChange}
-								value={certification.diploma}
-							/>
-							<button className="btn-add"
-								onClick={this.handleAdd}
-							>Add</button>
-						</div>
-					) : (
-						inputAddDocument.style.display = ''
-					)
-				}
+					diplomas.map((certification) => {
+						return <li key={uuid4()}>{certification.diploma}</li>
+					})
 
-			</div>
-		);
-	}
+				}
+			</ul>
+			{
+				(diplomas.length < 4) ? (
+					<div className="user-education-dic" id="listOfEducations">
+						<input type="text" name="education" placeholder="Add your Educations here"
+							onChange={handleChange}
+							value={certification.diploma}
+						/>
+						<button className="btn-add"
+							onClick={handleAdd}
+						>Add</button>
+					</div>
+				) : (
+					inputAddDocument.style.display = ''
+				)
+			}
+
+		</div>
+	);
 }
+
 
 export default Education;
