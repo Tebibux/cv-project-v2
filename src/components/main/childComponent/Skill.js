afa1d33a78a5b954e80f8dfc7f89e7def2a82cf5
tebibux <tebibusolomon79@gmail.com>
2023-04-25 09:27:28 +0300

    junk errors on changing to functional component

--- src/components/main/childComponent/Skill.js
+++ src/components/main/childComponent/Skill.js
@@ -1,61 +1,58 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uuid4 from "uuid4";
 
-class Skill extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			certification: { diploma: '' },
-			diplomas: [],
-		}
+
+const Skill = (props) => {
+	const { certification, setCertification } = useState('');
+	const inputAddDocument = document.getElementById('listOfSkill')
+
+	state = {
+		certification: { diploma: '' },
+		diplomas: [],
 	}
-	handleChange = (e) => {
-		this.setState({
+	const handleChange = (e) => {
+		setState({
 			certification: {
 				diploma: e.target.value,
 			}
 		})
 	}
-	handleAdd = (e) => {
+	const handleAdd = (e) => {
 		e.preventDefault();
-		this.setState({
-			diplomas: this.state.diplomas.concat(this.state.certification),
+		setState({
+			diplomas: state.diplomas.concat(state.certification),
 			certification: { diploma: '' }
 		})
 	}
-	render() {
-		const { certification, diplomas } = this.state;
-		const inputAddDocument = document.getElementById('listOfSkill')
-		return (
-			<div className="userSkill">
-				<div className="title">Skill</div>
-				<ul className="skills">
-					{
-						diplomas.map((certification) => {
-							return <li key={uuid4()}>{certification.diploma}</li>
-						})
 
-					}
-				</ul>
+	return (
+		<div className="userSkill">
+			<div className="title">Skill</div>
+			<ul className="skills">
 				{
-					(diplomas.length < 4) ? (
-						<div className="user-skill-dic" id="listOfSkill">
-							<input type="text" name="skills" placeholder="Add your Skills here"
-								onChange={this.handleChange}
-								value={certification.diploma}
-							/>
-							<button className="btn-add"
-								onClick={this.handleAdd}
-							>Add</button>
-						</div>
-					) : (
-						inputAddDocument.style.display = ''
-					)
+					diplomas.map((certification) => {
+						return <li key={uuid4()}>{certification.diploma}</li>
+					})
+
 				}
+			</ul>
+			{diplomas
+				(diplomas.length < 4) ? (
+				<div className="user-skill-dic" id="listOfSkill">
+					<input type="text" name="skills" placeholder="Add your Skills here"
+						onChange={handleChange}
+						value={certification.diploma}
+					/>
+					<button className="btn-add"
+						onClick={handleAdd}
+					>Add</button>
+				</div>
+			) : (
+				inputAddDocument.style.display = ''
+			)
+			}
 
-			</div>
-		);
-	}
+		</div>
+	)
 }
-
 export default Skill;
