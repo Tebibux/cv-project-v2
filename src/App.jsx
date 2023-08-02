3df0be29c6777fc568019332972379fab6acebc7
tebibux <tebibusolomon79@gmail.com>
2023-08-02 15:03:36 +0300

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -69,12 +69,22 @@ function App() {
                 <span> General Information </span> <FontAwesomeIcon icon={faChevronDown} />
               </button>
               {isShowPersonalInfo && <div className="personal-information drop-down-menu-list">
-                <div className='list'>UserPhoto</div>
-                <div className='list'>UserFullName</div>
-                <div className='list'>UserPhone</div>
-                <div className='list'>UserEmail</div>
-                <div className='list'>UserOtherLink</div>
-                <div className='list'>Location</div>
+                <div className='list UserPhoto input-container'>
+                  <input type="file" name="image" accept="image/jpeg, image/png" />
+                  <label htmlFor="file-input" className="custom-file-button">Upload Profile Image</label>
+                </div>
+                <div className='list UserFullName'>
+                  <input type="text" name="userName" id="" placeholder='Full Name' />
+                </div>
+                <div className='list UserPhone'>
+                  <input type="tel" name="userPhone" id="" placeholder='Phone Number' />
+                </div>
+                <div className='list UserEmail'>
+                  <input type="email" name="userEmail" id="" placeholder='Email' />
+                </div>
+                <div className='list Location'>
+                  <input type="text" name="userLocation" id="" placeholder='location' />
+                </div>
               </div>}
             </div>
 
@@ -85,9 +95,15 @@ function App() {
                 <span>Education Experience</span><FontAwesomeIcon icon={faChevronDown} />
               </button>
               {isShowEducation && <div className="education-information-list drop-down-menu-list">
-                <div className='list'>UserEducation</div>
-                <div className='list'>userDegree</div>
-                <div className='list'>userInstitute</div>
+                <div className='list UserEducation'>
+                  <input type="text" name="userEducation" id="" placeholder='Education' />
+                </div>
+                <div className='list userDegree'>
+                  <input type="text" name="degree" id="" placeholder='Degree, PhD ...' />
+                </div>
+                <div className='list userInstitute'>
+                  <input type="text" name="userInstitute" id="" placeholder='Yale University' />
+                </div>
                 <div className="add-item">
                   <button className="save">Save</button>
                   <div className="add-additional-item">
@@ -103,9 +119,23 @@ function App() {
                 <span> Professional Experience</span><FontAwesomeIcon icon={faChevronDown} />
               </button>
               {isShowExperiance && <div className="education-information-list drop-down-menu-list">
-                <div className='list'>userEmpDetail</div>
-                <div className='list'>userJobTitle</div>
-                <div className='list'>userEmpDurationYear</div>
+                <div className='list userEmpDetail'>
+                  <input type="text" name="userEmpDetail" id="" placeholder='Employed Company' />
+                </div>
+                <div className='list userJobTitle'>
+                  <input type="text" name="userJobTitle" id="" placeholder='Employment Position' />
+                </div>
+                <div className='list userEmpDurationYear'>
+                  <div className='employed-duration'>
+                    <label htmlFor="userEmpDurationYearFrom">Start</label>
+                    <input type="date" name="userEmpDurationYearFrom" id="" />
+                  </div>
+                  <div className='employed-duration'>
+                    <label htmlFor="userEmpDurationYearFrom">End</label>
+                    <input type="date" name="userEmpDurationYearTo" id="" />
+                  </div>
+                </div>
+                <div className='list'></div>
                 <div className="add-item">
                   <button className="save">Save</button>
                   <div className="add-additional-item">
@@ -120,7 +150,10 @@ function App() {
                 <span> Add Skill </span><FontAwesomeIcon icon={faChevronDown} />
               </button>
               {isShowSkill && <div className="education-information-list drop-down-menu-list">
-                <div className='list'>userSkill</div>
+                <div className='list'></div>
+                <div className='list userSkill'>
+                  <input type="text" name="userSkill" id="" placeholder='Photo Editing ...' />
+                </div>
                 <div className="add-item">
                   <button className="save">Save</button>
                   <div className="add-additional-item">
@@ -142,24 +175,46 @@ function App() {
               <li className='user-location' aria-label='user current location'> Wolayta, Sodo, Ethiopia</li>
             </ul>
           </div>
-          <div className="education-section">
-            <h4>Urban & Regional planning</h4>
-            <div className="education-desc">
-              <p>Hawassa University</p>
-              <p>September 2022 .E.C</p>
+          <div className="education-preview-section">
+            <h2>Education</h2>
+            <div className="education-review">
+              <h4>Urban & Regional planning <span className="edu-level"> Bachelor Degree</span></h4>
+              <div className="education-desc">
+                <p>Hawassa University</p>
+                <p>August 2016 - September 2022 .E.C</p>
+              </div>
+            </div>
+            <hr />
+            <div className="education-review">
+              <h4>Computer Science <span className="edu-level"> Bachelor Degree</span></h4>
+              <div className="education-desc">
+                <p>Rift Valley University</p>
+                <p>August 2016 - September 2022 .E.C</p>
+              </div>
             </div>
           </div>
           <div className="professional-exp">
-            <h4> Position</h4>
+            <h2>Experiance</h2>
             <div className="professional-desc">
-              <p>Company name</p>
-              <p>September 2023 E.C</p>
+              <p>Company Name: Sodo Municipality</p>
+              <p>Position: Planner</p>
+              <p>From September 2023 E.C to September 2023 E.C</p>
+            </div>
+            <hr />
+            <div className="professional-desc">
+              <p>Company Name: Awash Bank</p>
+              <p>Position: programmer</p>
+              <p>From September 2023 E.C to September 2023 E.C</p>
             </div>
           </div>
+
           <div className="skill-desc">
-            <p>Adobe PhotoShop</p>
-            <p>GIS</p>
-            <p></p>
+            <ul>
+              <li>Adobe PhotoShop</li>
+              <li>GIS</li>
+              <li>Adobe PhotoShop</li>
+              <li>GIS</li>
+            </ul>
           </div>
         </section>
       </main>
